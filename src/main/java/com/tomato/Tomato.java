435b95ea254819fc1547dcdf741ffa22306d54e3
Kunal Wanikar <kwanikar7@gmail.com>
2022-07-27 11:37:07 +0530

    Conflict Resolved

--- src/main/java/com/tomato/Tomato.java
+++ src/main/java/com/tomato/Tomato.java
@@ -1,11 +1,8 @@
 package com.tomato;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Tomato {
     public static void main(String[] args) {
-        SpringApplication.run(Tomato.class);
+
     }
 }
