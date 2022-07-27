6c2550f06f63e22e04030ff200637b82e9aaf1ab
Kunal Wanikar <kwanikar7@gmail.com>
2022-07-27 11:24:05 +0530

    Item Repo and Cart Details added

--- src/main/java/com/tomato/controller/CartController.java
+++ src/main/java/com/tomato/controller/CartController.java
@@ -2,7 +2,9 @@ package com.tomato.controller;
 
 import com.tomato.model.CartRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CartController {
 
 }
