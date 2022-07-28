687460da600623e4f44081abaf8b773fc632e0f0
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2022-07-28 17:38:07 +0530

    Update SearchService.java

--- src/main/java/com/tomato/service/SearchService.java
+++ src/main/java/com/tomato/service/SearchService.java
@@ -22,7 +22,7 @@ public class SearchService {
     public RestaurantDetail searchRestaurant(String name) {
         Restaurant restaurant = restaurantRepository.findByName(name);
 
-        List<Item> menuItems = itemRepository.findAllByrestaurantId(restaurant.getId());
+        List<Item> menuItems = itemRepository.findAllByRestaurantId(restaurant.getId());
 
         RestaurantDetail restaurantDetail = new RestaurantDetail();
 
