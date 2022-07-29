49af73f7794ddfa13052c5153e0d53dbb4dd7d3a
ISHANUJ HAZARIKA <71844260+Ishanuj99@users.noreply.github.com>
2022-07-29 12:13:55 +0530

    Update PaymentController.java

--- src/main/java/com/tomato/controller/PaymentController.java
+++ src/main/java/com/tomato/controller/PaymentController.java
@@ -3,9 +3,7 @@ package com.tomato.controller;
 import com.tomato.model.PriceDetail;
 import com.tomato.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
