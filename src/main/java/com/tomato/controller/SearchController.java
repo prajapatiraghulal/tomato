231317c115d4078267f8ebe51a8dcce9ca9db38b
ISHANUJ HAZARIKA <71844260+Ishanuj99@users.noreply.github.com>
2022-07-29 12:03:51 +0530

    Added @CrossOrigin

--- src/main/java/com/tomato/controller/SearchController.java
+++ src/main/java/com/tomato/controller/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class SearchController {
 
     @Autowired
