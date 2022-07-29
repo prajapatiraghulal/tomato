97237d4bfa468f2ed3749ff2de8097df050eaa1f
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2022-07-29 16:33:45 +0530

    Update LoginActivity.java

--- src/main/java/com/tomato/model/LoginActivity.java
+++ src/main/java/com/tomato/model/LoginActivity.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 @Entity
 public class LoginActivity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     private long tokenId;
     private String loginToken;
     private long userId;
